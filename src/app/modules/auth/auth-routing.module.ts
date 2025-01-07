31e3dc845c3874c034e3f438f17e5e42445c50e4
Biswajit Debnath <biswajit.sust@gmail.com>
2025-01-07 21:19:35 +0600

    profile page design changed

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -5,7 +5,7 @@ import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password
 import { NewPasswordComponent } from './pages/new-password/new-password.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { TwoStepsComponent } from './pages/two-steps/two-steps.component';
+import { TwoStepsComponent } from './pages/account-verification-two-steps/two-steps.component';
 import { LoginResolve } from 'src/app/core/resolver/login-resolve';
 
 const routes: Routes = [
